0ff5ae138c657df2f940042911e76ffd57c2d8da
RezzkzDA <rblxdava@gmail.com>
2024-12-18 19:25:27 +0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Zanzoken - A Minecraft Server',
+  description: 'Join Zanzoken Server, a simple and modern Minecraft server. Build, explore, and connect with players from around the world.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
